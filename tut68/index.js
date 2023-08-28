64ba243f1deea3e265ae5ae7ce9a701c9f081bc4
vamshi <vamshiprasa223@gmail.com>
2023-08-28 15:03:51 +0530

    added tut69

---
+++ tut68/index.js
@@ -0,0 +1,8 @@
+// const mod = require("./mod")
+// console.log(mod)
+
+// const average = require("./mod")
+// console.log(average([3,4]))
+const mod = require("./mod")
+console.log(mod.name)
+console.log("This is index.js")
---
+++ tut68/mod.js
@@ -0,0 +1,18 @@
+console.log("This is module")
+
+function average(arr){
+    let sum = 0
+    arr.forEach(element => {
+        sum+= element    
+    });
+
+    return  sum/arr.length;
+}
+
+// module.exports = {
+//     avg:average,
+//     name:"kittu",
+//     repo:"GitHub"
+// }
+
+module.exports.name = "kittu"
