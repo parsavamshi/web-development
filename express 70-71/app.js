c7fe9be0edbd5ff3fc3528303a639715972ab478
vamshi <vamshiprasa223@gmail.com>
2023-08-29 17:29:25 +0530

    added 74

--- express 70-71/app.js
+++ express 70-71/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 800;
+const fs = require("fs")
 
 //  EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
-
+app.use(express.urlencoded())
 
 //  PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template  engine as pug
@@ -16,6 +17,18 @@ app.get('/', (req, res)=>{
     const con= "This is the best content on the internet so far so use it wisely "
     const params = {'title':'pubG is the best game', "content":con}
     res.status(200).render('index.pug', params)
+})
+app.post("/", (req, res)=>{
+    // console.log(req.body)
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputTowrite = `the name of the client is ${name}, ${age} years old , ${gender}, residing at ${address}. More about him/her`
+    fs.writeFileSync('output.txt', outputTowrite)
+    const params = {"messgae":"Your form has been submitted successfully ", }
+    res.status(200).render("index.pug", params)
 })
 // START THE SERVER
 app.listen(port,()=>{
